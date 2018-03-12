752f60f541a10b78eeea4211af137a217367c7e1
Alexander Hans <mail@alexanderhans.me>
2018-03-12 10:19:54 +0100

    added set_env_keys_ahoi.bat

---
+++ demo_set_env_keys_ahoi.bat
@@ -0,0 +1,8 @@
+@echo off
+set ahoiInstallationId=ahoiInstallationId
+set ahoiClientId=ahoiClientId
+set ahoiClientSecret=ahoiClientSecret
+set bankUserName=bankUserName
+set bankUserPassword=bankUserPassword
+
+echo set ahoi environment variables for ruby script
